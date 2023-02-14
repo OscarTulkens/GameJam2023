d7eaf08aa3108553f991a9623ead75a04cf635ca
BendrGames <vanhaelst.ben@hotmail.com>
2023-02-14 20:43:38 +0100

    working on cinemachine shake

---
+++ GameJam2023_U/Assets/Scripts/game/CameraBehaviour/CinemachineShake.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+public class CinemachineShake : MonoBehaviour
+{
+    //public static CinemachineShake Instance { get; private set; }
+
+    //private CinemachineVirtualCamera cvc;
+    //private float shaketimer;
+    //private void Awake()
+    //{
+    //    Instance = this;
+    //    cvc = GetComponent<CinemachineVirtualCamera>();
+    //}
+
+    //public void shakeCamera(float intensity, float time)
+    //{
+    //    CinemachineBasicMultiChannelPerlin cmp = cvc.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+    //    cmp.m_AmplitudeGain = intensity;
+
+    //    shaketimer = time;
+    //}
+
+    //private void Update()
+    //{
+    //    if (shaketimer > 0)
+    //    {
+    //        shaketimer -= Time.deltaTime;
+    //        if (shaketimer <= 0f)
+    //        {
+    //            CinemachineBasicMultiChannelPerlin cmp = cvc.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+    //            cmp.m_AmplitudeGain = 0f;
+    //        }
+    //    }
+
+    //}
+}
